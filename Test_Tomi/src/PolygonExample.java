72777c313d20dba2240842ed58359ebcd2a4eeae
Claus <Claus@DESKTOP-P84MIGH>
2024-06-14 08:55:07 +0200

    commit

--- Test_Tomi/src/PolygonExample.java
+++ Test_Tomi/src/PolygonExample.java
@@ -1,10 +1,14 @@
+import java.util.List;
 
 public class PolygonExample {
 
 	public static void main(String[] args) {
 		PolygonReader polygonReader = new PolygonReader();
 		try {
-			System.out.println(polygonReader.importIt("daten.txt"));
+			List<Polygon> list = polygonReader.importIt("daten.txt");
+			System.out.println(list);
+			SvgWriter svgWriter = new SvgWriter();
+			//svgWriter.exportIt(list, "dummy.html");
 		} catch (ToFewPointsForPolygonException e) {
 			e.printStackTrace();
 		}
---
+++ Test_Tomi/src/SvgWriter.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class SvgWriter implements ExportPolygons {
+
+	@Override
+	public void exportIt(List<Polygon> listToExport, String filename) {
+		try {
+			String header = """
+					<!DOCTYPE html>
+					<html>
+					<body>
+					<h1>SVG Export</h1>
+					<svg width="300" height="300" axmlns="http://www.w3.org/2000/svg">
+					""";
+			Path path = Files.writeString(Path.of(filename), header, StandardOpenOption.CREATE_NEW);
+			listToExport.stream().forEach(p0 -> {
+				try {
+					StringBuilder polygon = new StringBuilder("<polygon stroke=\"black\" points=\"");
+					List<Point> points = p0.getPoints();
+					points.stream().forEach(p -> {
+						polygon.append(p.getX()).append(",").append(p.getY()).append(" ");
+					});
+					Files.writeString(path, polygon, StandardOpenOption.APPEND);
+				} catch (Exception e) {
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
